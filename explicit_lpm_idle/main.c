f069c47332b7d3fd90c8196520111082b74a1c9f
Michael Andersen <m.andersen@cs.berkeley.edu>
2016-02-02 16:31:42 -0800

    Some default applications to start

---
+++ explicit_lpm_idle/main.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2016 Michael Andersen <m.andersen@cs.berkeley.edu>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     powerprofile_tests
+ * @{
+ *
+ * @file
+ * @brief       power profiling example app
+ *
+ * @author      Michael Andersen <m.andersen@cs.berkeley.edu>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+#include <arch/lpm_arch.h>
+
+int main(void)
+{
+    lpm_arch_init();
+    lpm_arch_set(LPM_IDLE);
+
+    while(1);
+
+    return 0;
+}
